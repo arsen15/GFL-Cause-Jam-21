25065cc1437c2a08034fca29ee5e379a8092ddd6
Hayden Baldwin <haydenbaldwin9@gmail.com>
2024-04-12 15:00:26 -0400

    Add Damage Pop Up Text over enemies
    Uses a singleton generator to handle damage
    text creation.
    
    Later on, perhaps adjust size, and/or color,
    based on damage done as a percent of
    enemy max health.
    
    Followed this tutorial: https://www.youtube.com/watch?v=KOt85IoD__4&t=210s&ab_channel=ReForgeMode-UnityTutorials

---
+++ Assets/Scripts/DamagePopUpAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+public class DamagePopUpAnimation : MonoBehaviour
+{
+    public AnimationCurve opacityCurve;
+    public AnimationCurve scaleCurve;
+
+    public AnimationCurve heightCurve;
+
+    private float time = 0;
+    private TextMeshProUGUI tmp;
+
+    private Vector3 origin;
+    // Update is called once per frame
+
+    private void Awake(){
+        tmp = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        origin = transform.position;
+    }
+    private void Update()
+    {
+        tmp.color = new Color(1,1,1, opacityCurve.Evaluate(time)); 
+        transform.localScale = Vector3.one * scaleCurve.Evaluate(time);
+        transform.position = origin + new Vector3(0, 1 + heightCurve.Evaluate(time), 0);
+        time += Time.deltaTime;
+    }
+}
