5b9332bc770821c18dec14d8cd7e5990a5b6dfd7
Theo <95878514+TheoW03@users.noreply.github.com>
2024-04-23 13:40:00 -0400

    used prefab for UI :p

--- Assets/Scripts/DamagePopUpAnimation.cs
+++ Assets/Scripts/DamagePopUpAnimation.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class DamagePopUpAnimation : MonoBehaviour
 {
-    public AnimationCurve opacityCurve; //
+    public AnimationCurve opacityCurve; 
     public AnimationCurve scaleCurve;
 
     public AnimationCurve heightCurve;
