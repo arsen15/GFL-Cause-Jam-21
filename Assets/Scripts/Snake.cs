134932f3cfc09a01476e25bcc98fa4427a633947
Hayden Baldwin <haydenbaldwin9@gmail.com>
2024-04-26 12:52:45 -0400

    Refactor Sanke.cs to prepare for Spitting Action

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Diagnostics;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
@@ -22,6 +23,8 @@ public class Snake : MonoBehaviour
     public Transform[] patrolPoints;
     public float moveSpeed;
     public int patrolDestination;
+
+    public float spitWarmUpTime;
     public SpriteRenderer spriteRenderer;
     public Transform playerTransform;
     // Freeze Bullet interaction
@@ -37,6 +40,8 @@ public class Snake : MonoBehaviour
     private GameObject player;
     public int distanceFromPlayer;
     public float shootingPeriod;
+
+    public bool isShooting = false;
     // Start is called before the first frame update
 
     [SerializeField] float _InitialVelocity;
@@ -46,9 +51,9 @@ public class Snake : MonoBehaviour
     [SerializeField] float _Height;
     //private Camera cam;
 
-    public float hopDuration;
+    public float hopDurationPeriod;
 
-    public float hopCooldown;
+    public float hopCooldownPeriod;
 
     // Start is called before the first frame update
     void Start()
@@ -61,38 +66,17 @@ public class Snake : MonoBehaviour
     void Update()
     {      
         hopCooldownTimer += Time.deltaTime;
-        if (isFrozen)
-        {
-            return;
-        }
 
-        // Enemy patrol logic
-        if (hopCooldownTimer > hopCooldown && hopDurationTimer < hopDuration) {
+        if (isFrozen) return;
+
+        // Enemy hop and patrol logic
+        if (hopCooldownTimer > hopCooldownPeriod && hopDurationTimer < hopDurationPeriod) {
             hopDurationTimer += Time.deltaTime;
-            if (patrolDestination == 0)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
-                if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.5f)
-                {
-                    spriteRenderer.flipX = false;
-                    patrolDestination = 1;
-                }
-            }
-
-            if (patrolDestination == 1)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
-                if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.5f)
-                {
-                    spriteRenderer.flipX = true;
-                    patrolDestination = 0;
-                }
-            }
-        } else if (hopDurationTimer > hopDuration) {
+            DrawHopPatrolFrame();
+        } else if (hopDurationTimer > hopDurationPeriod) {
             hopCooldownTimer = 0;
             hopDurationTimer = 0;
         }
-
         
         float distance = Vector2.Distance(transform.position, player.transform.position);
         //Debug.Log(distance);
@@ -103,7 +87,7 @@ public class Snake : MonoBehaviour
 
             if (shootTimer > shootingPeriod)
             {
-                shoot();
+                StartCoroutine(ShootWithDelay());
                 shootTimer = 0;
             }
         }
@@ -158,7 +142,7 @@ public class Snake : MonoBehaviour
             {
                 bulletScript.InitializeBullet(false); // Ensure the bullet won't be destroyed on collision
             }
-            Debug.Log("Item dropped");
+            UnityEngine.Debug.Log("Item dropped");
         }
     }
 
@@ -203,6 +187,28 @@ public class Snake : MonoBehaviour
 
     }
 
+    private void DrawHopPatrolFrame() {
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.5f)
+            {
+                spriteRenderer.flipX = false;
+                patrolDestination = 1;
+            }
+        }
+
+        else if (patrolDestination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.5f)
+            {
+                spriteRenderer.flipX = true;
+                patrolDestination = 0;
+            }
+        }
+    }
+    
     // This is literally graphing it...
     IEnumerator CoroutineGraphVenom(GameObject venom, float v0, float _Angle, float time) {
         float t = 0;
@@ -214,9 +220,20 @@ public class Snake : MonoBehaviour
             t+= Time.deltaTime;
             yield return null;
         }
+        Destroy(venom);
     }
 
-
+    IEnumerator ShootWithDelay() {
+        isShooting = true;
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        yield return new WaitForSeconds(spitWarmUpTime);
+        shoot();
+        // Stop the stopwatch temporarily to log the time after the first operation
+        stopwatch.Stop();
+        UnityEngine.Debug.Log("Time after first operation: " + stopwatch.ElapsedMilliseconds + " ms");
+        isShooting = false;
+    }
+    
     void shoot()
     {
         GameObject newVenom = Instantiate(bullet, transform.position, Quaternion.identity);
@@ -227,12 +244,9 @@ public class Snake : MonoBehaviour
         float v0;
         float time;
         CalculatePathWithHeight(targetPos, height, out v0, out _Angle, out time);
-        //DrawPath(v0, _Angle, time, _Step);
-        //StopAllCoroutines();
         StartCoroutine(CoroutineGraphVenom(newVenom, v0, _Angle, time));
     
     }
 
-  
 
 }
